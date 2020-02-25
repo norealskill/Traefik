17e4069ba8ba442ab2b21d6daa511437b509a2fa
Clinton Cauley <clint.cauley@valdosta.edu>
2020-02-24 23:19:18 -0500

    grafana and prometheus almost there...

--- pushup.sh
+++ pushup.sh
@@ -1,4 +1,7 @@
+scp C:/Coding\ 2020/traefik/.env opc@150.136.163.94:~/traefik/.env
 scp C:/Coding\ 2020/traefik/*.* opc@150.136.163.94:~/traefik/
 scp C:/Coding\ 2020/traefik/dynamic/*.* opc@150.136.163.94:~/traefik/dynamic/
 scp C:/Coding\ 2020/traefik/graphql/*.* opc@150.136.163.94:~/traefik/graphql/
 scp C:/Coding\ 2020/traefik/prometheus/*.* opc@150.136.163.94:~/traefik/prometheus/
+scp C:/Coding\ 2020/traefik/grafana/*.* opc@150.136.163.94:~/traefik/grafana/
+scp C:/Coding\ 2020/traefik/grafana/.env opc@150.136.163.94:~/traefik/grafana/
